fbbd345f9600c37ecdf1a6ad9db7e4493e26a7a8
Fernando Ruiz <fernandoruizp97@gmail.com>
2020-03-15 17:23:10 +0100

    [fix] Cambiados metodos tras nueva query check parcela alquilada
    Se ha cambiado métodos del servicio de Smallholding tras cambiarse la
    query que devuelve el conjunto de parcelas alquiladas. También se ha
    realizado una modificación en los tests.

--- src/main/java/com/ispp/EcoRenter/repository/SmallholdingRepository.java
+++ src/main/java/com/ispp/EcoRenter/repository/SmallholdingRepository.java
@@ -1,7 +1,6 @@
 package com.ispp.EcoRenter.repository;
 
 import java.util.Collection;
-import java.util.Date;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -17,9 +16,6 @@ public interface SmallholdingRepository extends JpaRepository<Smallholding,Integ
 
     @Query("select sh from Smallholding sh where sh.isAvailable=true and sh.status = 'NO ALQUILADA'")
     Collection<Smallholding> findSmallholdingsAvailables();
-
-    @Query("select case when count(ro) > 0 then false else true end from RentOut ro where ro.renter.id = ?1 and ro.smallholding.id = ?2 and current_date() > ?3")
-    Boolean isSmallholdingRentedByRenter(int renterId, int smallholdingId, Date endDate);
     
     @Query("select distinct ro.smallholding from RentOut ro where ro.renter.id = ?1 and ro.isActive = true")
     Collection<Smallholding> findSmallholdingsByActiveRentOut(int renterId);
