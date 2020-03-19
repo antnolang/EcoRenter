bed61725f8ffeb410285280c610c55885a880222
alvcalgon <alvcalgon@alum.us.es>
2020-03-19 18:32:24 +0100

    [feat] Una version de los scripts

--- src/main/resources/schema-mysql.sql
+++ src/main/resources/schema-mysql.sql
@@ -130,10 +130,11 @@ DROP TABLE IF EXISTS `photo`;
 CREATE TABLE `photo` (
   `id` int(11) NOT NULL,
   `version` int(11) NOT NULL,
-  `content` varchar(255) DEFAULT NULL,
+  `data` longblob,
   `name` varchar(255) DEFAULT NULL,
-  `structure` longblob,
   `suffix` varchar(255) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `structure` longblob,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -298,4 +299,4 @@ CREATE TABLE `valuation` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-15 16:18:21
+-- Dump completed on 2020-03-19 18:31:31
