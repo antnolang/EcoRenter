3f3f62f8906f6f5338a9edf9eefa5126091cb2a7
Julgargal <43671631+julgargal@users.noreply.github.com>
2020-04-19 12:49:32 +0200

    solucion conflicto

--- src/main/java/com/ispp/EcoRenter/service/MailService.java
+++ src/main/java/com/ispp/EcoRenter/service/MailService.java
@@ -4,85 +4,49 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
-import com.ispp.EcoRenter.model.Actor;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import com.ispp.EcoRenter.model.Actor;
 
 @Service
 @Transactional
 public class MailService {
 
+    @Autowired
+    private JavaMailSender mailSender;
 
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        
-        mailSender.setUsername("ecorenter@gmail.com");
-        mailSender.setPassword("ecorenter2020.");
-        
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-        
-        return mailSender;
-    }
+    protected void sendEmail(final List<Actor> recipients, final String subject, final String content) {
 
-    protected void sendEmail(List<Actor> recipients, String subject, String content) {
+	for (Actor a : recipients) {
+	    SimpleMailMessage email = new SimpleMailMessage();
+	    email.setTo(a.getEmail());
+	    email.setSubject(subject);
+	    email.setText(content);
 
-        for(Actor a: recipients){
-            SimpleMailMessage email = new SimpleMailMessage();
-            email.setTo(a.getEmail());
-            email.setSubject(subject);
-            email.setText(content);
-        
-            this.getJavaMailSender().send(email);
-        }
-        
-    }
-
-
-	@Autowired
-	private JavaMailSender mailSender;
-
-	protected void sendEmail(List<Actor> recipients, String subject, String content) {
-
-		for(Actor a: recipients){
-			SimpleMailMessage email = new SimpleMailMessage();
-			email.setTo(a.getEmail());
-			email.setSubject(subject);
-			email.setText(content);
-
-			mailSender.send(email);
-		}
-
+	    this.mailSender.send(email);
 	}
 
+    }
 
-	public void sendEmailDispute(Actor recipient, Actor receiver,String admin, String tipo, String descripcion,int smallholdingId) {
+    public void sendEmailDispute(final Actor recipient, final Actor receiver, final String admin, final String tipo,
+	    final String descripcion, final int smallholdingId) {
 
-		SimpleMailMessage email = new SimpleMailMessage();
+	SimpleMailMessage email = new SimpleMailMessage();
 
-		email.setFrom(recipient.getEmail());
-		email.setTo(admin);
-		email.setCc(receiver.getEmail());
-		email.setSubject(tipo);
-		email.setText("El creador de esta disputa es: "+ recipient.getUserAccount().getUsername()+"\n"
-				+ "El involucrado en esta disputa es: "+ receiver.getUserAccount().getUsername()+"\n"
-				+"Siendo la id de la parcela en disputa la siguiente: " + smallholdingId +"\n"
-				+"Siendo el texto descrito por el creador de la disputa el siguiente: "+"\n"
-				+ descripcion);
+	email.setFrom(recipient.getEmail());
+	email.setTo(admin);
+	email.setCc(receiver.getEmail());
+	email.setSubject(tipo);
+	email.setText("El creador de esta disputa es: " + recipient.getUserAccount().getUsername() + "\n"
+		+ "El involucrado en esta disputa es: " + receiver.getUserAccount().getUsername() + "\n"
+		+ "Siendo la id de la parcela en disputa la siguiente: " + smallholdingId + "\n"
+		+ "Siendo el texto descrito por el creador de la disputa el siguiente: " + "\n" + descripcion);
 
-		mailSender.send(email);
+	this.mailSender.send(email);
 
-	}
-
+    }
 
 }
